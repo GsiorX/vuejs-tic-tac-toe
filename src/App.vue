7649debe34dcb1b0810195b559a194a84a677f67
kubagas <jakub.gasiorek@gmail.com>
2019-01-15 00:06:37 +0100

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,198 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="centered">
+			<label for="cells">Rozmiar (nie mniejszy niż 3)</label>
+			<input id="cells" type="number" v-model="cells"/>
+		</div>
+
+		<br><br><br><br>
+
+		<div class="cellsArea" v-if="!reset">
+			<div v-for="(cell, row) in cellArray" :key="row">
+				<div v-for="(obj, column) in cell" :key="row+column" class="cells">
+					<cell-box :row="row" :column="column" :point="value" @clicked="clicked"></cell-box>
+				</div>
+				<br>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import CellBox from './components/Cell.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'app',
+
+		components: {
+			CellBox
+		},
+
+		data() {
+			return {
+				cells: 3,
+				cellArray: [],
+				turn: 0,
+				value: [],
+				reset: false,
+			}
+		},
+
+		created() {
+			this.buildCellArray();
+		},
+
+		methods: {
+			// Odbieramy event od dziecka
+			clicked: function(data) {
+				let result = true;
+				let point = '';
+
+				if (this.turn % 2 === 0) {
+					point = 'X';
+					result = this.assignValue(data.row, data.column, point);
+				}
+				else {
+					point = 'O';
+					result = this.assignValue(data.row, data.column, point);
+				}
+
+				if (result) {
+					this.turn++;
+
+					this.value = [data.row, data.column, point];
+
+					this.checkWinner();
+				}
+			},
+
+			assignValue: function(row, column, value) {
+				if (this.cellArray[row][column] === null) {
+					this.cellArray[row][column] = value;
+
+					return true;
+				}
+
+				return false;
+			},
+
+			buildCellArray: function() {
+				this.reset = true;
+				this.turn = 0;
+				this.cellArray = [];
+
+				for (let i = 0; i < this.cells; i++) {
+					this.cellArray[i] = [];
+
+					for (let j = 0; j < this.cells; j++) {
+						this.cellArray[i][j] = null;
+					}
+				}
+
+				this.$nextTick().then(() => {
+					this.reset = false;
+				});
+			},
+
+			checkWinner: function() {
+				for (let i = 0; i < this.cells; i++) {
+					let rowSum = 0;
+					for (let j = 0; j < this.cells; j++) {
+						if (this.cellArray[i][j] === 'X') {
+							rowSum++;
+						}
+						else if (this.cellArray[j][i] === 'O') {
+							rowSum--;
+						}
+					}
+
+					if (rowSum === this.cells) {
+						this.win('X');
+					}
+					else if (rowSum === -this.cells) {
+						this.win('O');
+					}
+				}
+
+				for (let i = 0; i < this.cells; i++) {
+					let colSum = 0;
+					for (let j = 0; j < this.cells; j++) {
+						if (this.cellArray[j][i] === 'X') {
+							colSum++;
+						}
+						else if (this.cellArray[j][i] === 'O') {
+							colSum--;
+						}
+					}
+
+					if (colSum === this.cells) {
+						this.win('X');
+					}
+					else if (colSum === -this.cells) {
+						this.win('O');
+					}
+				}
+
+				let diagonal = 0;
+
+				for (let i = 0; i < this.cells; i++) {
+					if (this.cellArray[i][i] === 'X') {
+						diagonal++;
+					}
+					else if (this.cellArray[i][i] === 'O') {
+						diagonal--;
+					}
+				}
+
+				if (diagonal === this.cells) {
+					this.win('X');
+				}
+				else if (diagonal === -this.cells) {
+					this.win('O');
+				}
+			},
+
+			win: function(winner) {
+				alert(`${winner} wygrywa!`);
+
+				this.buildCellArray();
+			}
+		},
+
+		watch: {
+			cells: function() {
+				//Zeby nie byl stringiem
+				this.cells = JSON.parse(this.cells);
+
+				if (this.cells < 3) {
+					this.cells = 3;
+				}
+
+				this.buildCellArray();
+			},
+
+			turn: function() {
+				if(this.turn === Math.pow(this.cells, 2)) {
+					alert('Brak zwyciezcy');
+					this.buildCellArray();
+				}
+			}
+		},
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+	.centered {
+		text-align: center;
+		overflow: hidden;
+	}
+
+	.cellsArea {
+		justify-content: center;
+		margin: 0 auto;
+	}
+
+	.cells {
+		display: inline-block;
+	}
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="cell" @click="clicked">
+		{{ value }}
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'cell-box',
+
+		props: {
+			row: {
+				type: Number,
+				required: true,
+			},
+
+			column: {
+				type: Number,
+				required: true,
+			},
+
+			point: {
+				type: Array,
+				required: false,
+			}
+		},
+
+		data() {
+			return {
+				value: '',
+			}
+		},
+
+		methods: {
+			clicked: function() {
+				// Wysyłamy event do parenta przy kliknięciu na pole
+				this.$emit('clicked', {
+					row: this.row,
+					column: this.column,
+				});
+			}
+		},
+
+		watch: {
+			point: function() {
+				let point = this.point;
+
+				if (point[0] !== undefined && point[1] !== undefined && point[2] !== undefined) {
+					if (point[0] === this.row && point[1] === this.column) {
+						this.value = point[2];
+					}
+				}
+			}
+		}
+	}
+</script>
+
+
+<style scoped>
+	.cell {
+		height: 100px;
+		width: 100px;
+		margin: 2px;
+		border: 1px solid black;
+		line-height: 90px;
+		text-align: center;
+		font-size: 50px;
+	}
+</style>
